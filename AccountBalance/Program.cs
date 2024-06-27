ccfc32a9259ed8fa4127c4ef7aeced56be008412
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-27 14:32:32 +0300

    Initial commit

---
+++ AccountBalance/Program.cs
@@ -0,0 +1,27 @@
+﻿class AccountBalance
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        decimal balance = 0;
+
+        while (input!= "End")
+        {
+            decimal amount = decimal.Parse(input);
+            
+            if (amount > 0)
+            {
+                Console.WriteLine($"Increase: {amount:F2}");
+                balance += amount;
+            }
+            else
+            {
+                Console.WriteLine($"Decrease: {Math.Abs(amount):F2}");
+                balance += amount;
+                
+            }
+            input = Console.ReadLine();
+        }
+        Console.WriteLine($"Balance: {balance:F2}");
+    }
+}
